ca7b4266deaf1bb9867bb71d27f8ed7a264eaa7a
ishak-dev <ishak.kazic@gmail.com>
2022-12-04 04:44:55 +0100

    Forms for doctor login

--- doctor/forms.py
+++ doctor/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from doctor.login import User
+from doctor.models import User
 
 
 class LoginForm(FlaskForm):
